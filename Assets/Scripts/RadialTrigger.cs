f7f3b26f54115b4630e986b50c7395504f6623be
KonstantinosVerykios <verykioskonstantinos@gmail.com>
2026-01-30 11:55:20 +0200

    Completed Triggers week assignment

--- Assets/Scripts/RadialTrigger.cs
+++ Assets/Scripts/RadialTrigger.cs
@@ -1,20 +1,25 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
 public class Triggers : MonoBehaviour
 {
-    [Range(4f, 10f)]
+    [Range(4f, 100f)]
     public float Radius = 1f;
 
     // If the range is -1 the enemy can see behind him
     [Range(-1f, 1f)]
     public float EnemyFOV = 0.7f;
 
+    [Range(0f, 10f)]
+    public float ConeHeight = 0.7f;
+
     private float LookingAngle;
+    
 
     public GameObject Target;
     public GameObject LookAt;
@@ -28,16 +33,21 @@ public class Triggers : MonoBehaviour
     private void OnDrawGizmos()
     {
         SetYZero(LookAt);
-        SetYZero(Target);
+
 
         bool in_range = InRange();
-        if (in_range)
+        bool looking = LookingAt();
+        if (in_range && looking)
+        {
+            Handles.color = Color.red;
+        }
+        else if (in_range)
         {
             Handles.color = Color.yellow;
         }
-        else 
+        else
         {
-            Handles.color = Color.red;
+            Handles.color = Color.white;
         }
 
         //XZ-plane, wirediscs normal is Y-up
@@ -64,18 +74,50 @@ public class Triggers : MonoBehaviour
 
         // The right edge of the enemys FOV
         Vector3 look_at_rotated = rotator * to_lookAt;
-        Drawing.DrawVector(look_at_rotated, transform.position, Color.blue, 3f, true);
 
+        
+        //Drawing.DrawVector(look_at_rotated, transform.position, Color.blue, 3f, true);
 
         // The left edge of the enemys FOV by inverting look_at_rotated
         look_at_rotated = Quaternion.Inverse(rotator) * to_lookAt;
-        Drawing.DrawVector(look_at_rotated, transform.position, Color.blue, 3f, true);
+        //Drawing.DrawVector(look_at_rotated, transform.position, Color.blue, 3f, true);
 
         // Creates a wirearc form the left edges point to the center* 2
-        Handles.DrawWireArc(transform.position, Vector3.up, look_at_rotated, LookingAngle * 2, to_lookAt.magnitude);
+        //Handles.DrawWireArc(transform.position, Vector3.up, look_at_rotated, LookingAngle * 2, to_lookAt.magnitude);
+        
 
+        // WEEKLY ASSIGNMENT
 
-        // WEEK ASSIGNMENT
+        Vector3 origin = transform.position;
+        Vector3 forward = to_lookAt.normalized * look_at_rotated.magnitude;
+
+        Quaternion leftRot = Quaternion.AngleAxis(LookingAngle, Vector3.down);
+        Quaternion rightRot = Quaternion.AngleAxis(LookingAngle, Vector3.up);
+
+        Vector3 left = leftRot * forward;
+        Vector3 right = rightRot * forward;
+
+        Handles.color = Color.white;
+
+        // Base triangle
+        Handles.DrawLine(origin, origin + left);
+        Handles.DrawLine(origin, origin + right);
+        //Handles.DrawLine(origin + left, origin + right);
+        Handles.DrawWireArc(origin, Vector3.up, look_at_rotated, LookingAngle * 2, forward.magnitude);
+
+        // "Support Pillars"
+        Vector3 up = Vector3.up * ConeHeight;
+
+        Handles.DrawLine(origin, origin + up);
+        Handles.DrawLine(origin + left, origin + left + up);
+        Handles.DrawLine(origin + right, origin + right + up);
+        
+
+        // Top triangle
+        Handles.DrawLine(origin + up, origin + left + up);
+        Handles.DrawLine(origin + up, origin + right + up);
+        //Handles.DrawLine(origin + left + up, origin + right + up);
+        Handles.DrawWireArc(origin + up, Vector3.up, look_at_rotated, LookingAngle * 2, forward.magnitude); ;
 
     }
 
@@ -83,7 +125,7 @@ public class Triggers : MonoBehaviour
     {
         // End point - start point = the vector that leads to it
         to_target = Target.transform.position - transform.position;
-        to_target.y = 0f; // Make the plane flat so everything is on y = 0
+        // Make the plane flat so everything is on y = 0
 
         // If the vector is smaller then the radius the player is inside the radius
         if(to_target.magnitude < Radius)
@@ -97,7 +139,7 @@ public class Triggers : MonoBehaviour
         Vector3 to_targetN = to_target.normalized;
         Vector3 to_lookAtN = to_lookAt.normalized;
 
-        if(Vector3.Dot(to_lookAtN, to_targetN) > EnemyFOV )
+        if(Vector3.Dot(to_lookAtN, to_targetN) > EnemyFOV && to_target.y < ConeHeight && to_target.y >=0)
             return true;
         else 
             return false;
